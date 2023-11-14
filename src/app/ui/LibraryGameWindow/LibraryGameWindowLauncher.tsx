e6f9538b1de6237502dc3223433353960a9b1896
Alex <techno199@live.ru>
2023-11-14 07:17:30 +0300

    main

--- src/app/ui/LibraryGameWindow/LibraryGameWindowLauncher.tsx
+++ src/app/ui/LibraryGameWindow/LibraryGameWindowLauncher.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Button from "@/ui/Button/Button";
 import {GameLibraryStore, LibrarySignal} from "@/app/signal";
 import {observer} from "mobx-react";
-import {motion} from "framer-motion";
+import {AnimatePresence, motion} from "framer-motion";
 
 export type LibraryGameWindowLauncherProps = {}
 
@@ -11,24 +11,28 @@ const LibraryGameWindowLauncher = observer((props: LibraryGameWindowLauncherProp
   const {selectedGame} = GameLibraryStore;
 
   return (
-    <motion.div
-      initial={{y: 20}}
-      animate={{y: 0}}
-      transition={{type: 'just'}}
-      className={'flex flex-col px-1 grow mt-[300px]'}
-    >
-      <div className={'flex flex-col grow px-4 backdrop-blur'}>
-        {/* 1st ROW */}
-        <div className={'flex gap-4'}>
-          <a href={selectedGame.gameUrl} target={'_blank'}>
-            <Button color={'accent-8'} className={'px-14 py-2.5 !text-lg font-medium'}>
-              PLAY
-            </Button>
-          </a>
+      <div className={'flex flex-col px-1 grow pt-[300px]'}>
+        <div className={'flex flex-col grow px-4 backdrop-blur'}>
+          <AnimatePresence mode={'popLayout'}>
+            <motion.div
+              key={selectedGame.name}
+              initial={{y: 20, opacity: 0, }}
+              animate={{y: 0, opacity: 1}}
+              exit={{y: -5, opacity: 0}}
+              transition={{type: 'just'}}
+            >
+              {/* 1st ROW */}
+              <div className={'flex gap-4'}>
+                <a href={selectedGame.gameUrl} target={'_blank'}>
+                  <Button color={'accent-8'} className={'px-14 py-2.5 !text-lg font-medium'}>
+                    PLAY
+                  </Button>
+                </a>
+              </div>
+            </motion.div>
+          </AnimatePresence>
         </div>
-
       </div>
-    </motion.div>
   );
 });
 
